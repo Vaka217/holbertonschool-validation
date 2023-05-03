80fb7b0262069434b1e836904dc7e06bb7896bc9
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-05-03 05:43:25 -0700

    Fix docker start

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,4 +1,6 @@
-#!bin/bash
+service docker start
 docker run --name setup --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-make build
+docker exec setup apt-get update
+docker exec setup apt-get install -y hugo make
+docker exec make build
 exit
