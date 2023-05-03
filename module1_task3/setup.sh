0f9e817a724b3c525262333185f015f5f297958c
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-05-03 05:59:03 -0700

    remove docker run from script

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,7 +1,3 @@
-sudo dockerd
-service docker start
-docker run --name setup --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-docker exec setup apt-get update
-docker exec setup apt-get install -y hugo make
-docker exec make build
-exit
+#!/bin/bash
+apt-get update && apt-get install -y hugo make
+make build
