9ff50cab979f7a5863a740f487e0bb2b6afa087e
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-05-03 06:23:52 -0700

    add mv

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 apt-get update
+apt-get install make curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz -o hugo.tar.gz
 tar -xvzf hugo.tar.gz
 mv hugo /usr/local/bin/
+rm 
 make build
