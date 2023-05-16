54811e43ab1d7aabc5498ea82f49c4dc3e914021
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-05-16 08:36:16 -0700

    added golang lint install

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-apt-get update
-curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
-tar xzf hugo.tar.gz hugo
-mv hugo /usr/local/bin/
-apt-get install -y hugo make
-npm install markdownlint
-npm install markdown-link-check
-make build
+sudo apt-get update
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s
+sudo curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
+sudo tar xzf hugo.tar.gz hugo
+sudo mv hugo /usr/local/bin/
+sudo apt-get install -y hugo make
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
+sudo make build
