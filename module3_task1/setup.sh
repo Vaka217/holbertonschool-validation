249e255a858eccfd1b26a85c7312d5eebe09b2b3
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-05-16 07:28:58 -0700

    Task 1

---
+++ module3_task1/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get update
+curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
+tar xzf hugo.tar.gz hugo
+mv hugo /usr/local/bin/
+apt-get install -y hugo make
+npm install markdownlint
+npm install markdown-link-check
+make build
