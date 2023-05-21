82f059157acc15af3a87541deb7c5fab98d21c46
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-05-21 11:30:57 -0300

    Task 0

---
+++ module4_task0/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update
+sudo curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
+sudo tar xzf hugo.tar.gz hugo
+sudo mv hugo /usr/local/bin/
+sudo apt-get install -y hugo make
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
