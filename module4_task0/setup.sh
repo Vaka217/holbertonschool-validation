ce077eb770430f576cd6de1c81fa5083b9026d36
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-05-21 12:12:28 -0300

    Change setup

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo docker pull golang:1.15.8-buster
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s
 sudo curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
 sudo tar xzf hugo.tar.gz hugo
 sudo mv hugo /usr/local/bin/
